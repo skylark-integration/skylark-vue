e6cfec82879af87733c05282eb0e98fff39305a3
hudaokeji <github@hudaokeji.com>
2018-12-24 16:49:31 +0900

    init commit

---
+++ dist/skylark-vue/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-vue - A version of vuejs framework that ported to running on skylarkjs.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-vue/
+ * @license MIT
+ */
+define(["./vue"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ src/main.js
@@ -0,0 +1,5 @@
+define([
+	"./vue"
+],function(vue){
+	return vue;
+});
